981a73b14b92922931fd28f91fa3ad2984c3267b
Flight-of-Dragons <jeremy.barrass@gmail.com>
2016-06-29 16:39:03 +0100

    All current feature and unit tests passing for photo upload: existence of frontend elements and controller function.

--- tests/e2e-tests/uploadPhoto.test.js
+++ tests/e2e-tests/uploadPhoto.test.js
@@ -1,13 +1,21 @@
 "use strict";
 
 describe('uploading a photo', function(){
+
+	var uploadButton, selectFromGalleryButton, takeWithCameraButton;
+
 	beforeEach(function(){
 		browser.get('/#/side-menu21/uploadPhoto');
 		uploadButton = element(by.id('upload'));
 		selectFromGalleryButton = element(by.id('gallery'));
+		takeWithCameraButton = element(by.id('camera'));
 	});
 
 	it('should have a button to select a photo stored on the device', function(){
 		expect(selectFromGalleryButton).toBeDefined();
+	});
+
+	it('should have a button to take a photo with the device camera', function(){
+		expect(takeWithCameraButton).toBeDefined();
 	});
 });

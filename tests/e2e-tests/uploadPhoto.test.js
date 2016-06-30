cd8e7e0eec557f5af900365db10f83e460279ee2
Flight-of-Dragons <jeremy.barrass@gmail.com>
2016-06-30 14:27:28 +0100

    Feature test to add photo to upload photo page written.  Currently failing with "Failed Expectation". Researching best design for adding photos from a device in Ionic framework.  Cordova plugins required.

--- tests/e2e-tests/uploadPhoto.test.js
+++ tests/e2e-tests/uploadPhoto.test.js
@@ -2,13 +2,14 @@
 
 describe('uploading a photo', function(){
 
-	var uploadButton, selectFromGalleryButton, takeWithCameraButton;
+	var uploadButton, selectFromGalleryButton, takeWithCameraButton, staffAvatar;
 
 	beforeEach(function(){
 		browser.get('/#/side-menu21/uploadPhoto');
 		uploadButton = element(by.id('upload'));
 		selectFromGalleryButton = element(by.id('gallery'));
 		takeWithCameraButton = element(by.id('camera'));
+		staffAvatar = element(by.id('avatar'));
 	});
 
 	it('should have a button to select a photo stored on the device', function(){
@@ -17,5 +18,12 @@ describe('uploading a photo', function(){
 
 	it('should have a button to take a photo with the device camera', function(){
 		expect(takeWithCameraButton).toBeDefined();
+	});
+
+	it('should display a photo when one is selected from the "Photo Gallery"', function(){
+		var drtinycat = 'img/jstdrtinycat.jpg';
+		selectFromGalleryButton.click().then(function(){	
+			expect(staffAvatar).toMatch(drtinycat);
+		});
 	});
 });
